6278c8737bf6a2ae8b95098e0f33243ec3f9677b
Claude Meurehg <claude.meurehg@mail.utoronto.ca>
2018-05-05 10:00:05 -0400

    All working well, except the reset.  Player needs to click on button everytime he or she wants to play. Counters continue ok.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,14 +13,16 @@ $(document).ready(function(){
 
     var valuesForCoins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     
+    
 
     for (i=0; i < 4; i++) {
 
         var coinSet = $("<img>");
-
+        var valuesForCoinsR = Math.floor(Math.random() * 12) + 1;
+    
         coinSet.addClass("coins-photo");
         coinSet.attr("src", "assets/images/gold-coins.jpg");
-        coinSet.attr("data-value", valuesForCoins[i]);
+        coinSet.attr("data-value", valuesForCoinsR);
         coins.append(coinSet);
 
     }
